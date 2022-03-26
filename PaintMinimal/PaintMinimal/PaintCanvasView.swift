6a7fabf9c1e2d7d0e0169649bc9235749dc96739
SecondThundeR <awayfromgalaxy@gmail.com>
2022-03-26 12:36:13 +0300

    Rename ContentView to PaintCanvasView

--- PaintMinimal/PaintMinimal/PaintCanvasView.swift
+++ PaintMinimal/PaintMinimal/PaintCanvasView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  PaintCanvasView.swift
 //  PaintMinimal
 //
 //  Created by Владислав on 26.03.2022.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PaintCanvasView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct PaintCanvasView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        PaintCanvasView()
     }
 }
