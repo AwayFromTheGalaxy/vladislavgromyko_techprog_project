c4e1af7eb7680cf272e47bdcd97e91f927dcc4c8
SecondThundeR <awayfromgalaxy@gmail.com>
2022-03-26 12:23:53 +0300

    Init "PaintMinimal" project

---
+++ PaintMinimal/PaintMinimal/PaintMinimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintMinimalApp.swift
+//  PaintMinimal
+//
+//  Created by Владислав on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct PaintMinimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
