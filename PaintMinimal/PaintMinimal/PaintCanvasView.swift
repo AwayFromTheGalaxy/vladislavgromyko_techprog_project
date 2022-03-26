7a7842c3b450eb1c8e0abefee00f8c7a1f2eb3fa
SecondThundeR <awayfromgalaxy@gmail.com>
2022-03-26 12:49:50 +0300

    Add PaintEngine and Buttons for drawing history

--- PaintMinimal/PaintMinimal/PaintCanvasView.swift
+++ PaintMinimal/PaintMinimal/PaintCanvasView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct PaintCanvasView: View {
     @State private var lines = [PaintLine]()
+    @State private var deletedLines = [PaintLine]()
+
     @State private var selectedColor: Color = .black
     @State private var selectedLineWidth: CGFloat = 1
+    
+    let paintEngine = PaintEngine()
 
     var body: some View {
         VStack {
@@ -21,6 +25,23 @@ struct PaintCanvasView: View {
                     Text("linewidth")
                 }.frame(maxWidth: 100)
                 Text(String(format: "%.0f", selectedLineWidth))
+                
+                Button {
+                    let last = lines.removeLast()
+                    deletedLines.append(last)
+                } label: {
+                    Image(systemName: "arrow.uturn.backward.circle")
+                        .imageScale(.large)
+                }.disabled(lines.count == 0)
+                
+                Button {
+                    let last = deletedLines.removeLast()
+                    
+                    lines.append(last)
+                } label: {
+                    Image(systemName: "arrow.uturn.forward.circle")
+                        .imageScale(.large)
+                }.disabled(deletedLines.count == 0)
             }.padding()
 
             ZStack {
