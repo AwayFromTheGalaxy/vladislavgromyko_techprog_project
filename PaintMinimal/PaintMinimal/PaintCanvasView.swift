db723cfd6cd8d4a587b51c0f0c6fb2a75730074e
SecondThundeR <awayfromgalaxy@gmail.com>
2022-03-26 12:47:37 +0300

    Add Slider Control for line width

--- PaintMinimal/PaintMinimal/PaintCanvasView.swift
+++ PaintMinimal/PaintMinimal/PaintCanvasView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct PaintCanvasView: View {
     @State private var lines = [PaintLine]()
     @State private var selectedColor: Color = .black
+    @State private var selectedLineWidth: CGFloat = 1
 
     var body: some View {
         VStack {
             HStack {
                 ColorPicker("Color Palette", selection: $selectedColor)
                     .labelsHidden()
+                Slider(value: $selectedLineWidth, in: 1...20) {
+                    Text("linewidth")
+                }.frame(maxWidth: 100)
+                Text(String(format: "%.0f", selectedLineWidth))
             }.padding()
 
             ZStack {
